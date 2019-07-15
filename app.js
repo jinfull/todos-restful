74ebbcb7efb5fd142d97bebb987f119ec00abeea
jinfull <jinfull@gmail.com>
2019-07-15 11:48:53 -0700

    Add update todo route

--- app.js
+++ app.js
@@ -84,6 +84,55 @@ app.delete('/api/v1/todos/:id', (req, res) => {
   });
 });
 
+// update a todo
+app.put('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo updated successfully',
+    updatedTodo,
+  });
+});
+
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+
+
